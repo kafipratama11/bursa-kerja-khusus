514067c6137a12c83e5152ee1b3333df7f776c04
kafipratama11 <kafipratama0606@gmail.com>
2024-03-17 00:38:17 +0700

    k

---
+++ database/seeders/PermissionSeeder.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Permission;
+use Spatie\Permission\Models\Role;
+
+class PermissionSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $role_admin = Role::updateOrCreate([
+            'name' => 'admin',
+        ],
+        ['name' => 'admin']
+    );
+        $role_user = Role::updateOrCreate([
+        'name' => 'user',
+        ],
+        ['name' => 'user']
+        );
+        
+        $permission1 = Permission::updateOrCreate(
+            [
+                'name' => 'view_dashboard',
+            ],
+            ['name' => 'view_dashboard']
+        );
+        $permission2 = Permission::updateOrCreate(
+            [
+                'name' => 'view_index',
+            ],
+            ['name' => 'view_index']
+        );
+
+        $role_admin->givePermissionTo($permission1);
+        $role_admin->givePermissionTo($permission2);
+        $role_user->givePermissionTo($permission2);
+
+        $user = User::find(1);
+        $user2 = User::find(2);
+
+        $user->assignRole('admin');
+        $user2->assignRole('user');
+    }
+}
--- resources/views/employer-dashboard.blade.php
+++ resources/views/employer-dashboard.blade.php
@@ -1,6 +1,5 @@
 @extends('layouts.app')
 @section('content')
-@include('partials.navbar-tiga')
 <div class="container">
       <h1 class="text-primary fs-1">hello world</h1>
 </div>
