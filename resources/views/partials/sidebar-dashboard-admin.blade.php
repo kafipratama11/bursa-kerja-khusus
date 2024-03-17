218bbb6b40de61952f08d850c2c810966d133222
kafipratama11 <kafipratama0606@gmail.com>
2024-03-17 15:14:53 +0700

    sidebar-dashboard

--- resources/views/partials/sidebar-dashboard-admin.blade.php
+++ resources/views/partials/sidebar-dashboard-admin.blade.php
@@ -10,7 +10,7 @@
       <div class="px-3 ">
             <hr style="color: #8ca9e0;">
       </div>
-      <div class="ps-3  mb-3 text-dark">Menu lainnya</div>
+      <div class="ps-3  mb-3 text-dark fw-bolder menu-sidebar">MENU</div>
       <a href="" class="link-underline link-underline-opacity-0 text-dark nav-link-sidebar initialColor colorLink" data-page="page1" id="colorLink">
             <div class="p-2 ps-4">
                   <div class="d-flex gap-3">
@@ -23,7 +23,7 @@
             <div class="p-2 ps-4">
                   <div class="d-flex gap-3">
                         <i class="bi bi-grid-1x2 text-dark"></i>
-                        Users
+                        Companys
                   </div>
             </div>
       </a>
@@ -31,11 +31,21 @@
             <div class="p-2 ps-4">
                   <div class="d-flex gap-3">
                         <i class="bi bi-grid-1x2 text-dark"></i>
-                        Users
+                        Lowongan Pekerjaan
                   </div>
             </div>
       </a>
       <div class="px-3 ">
             <hr style="color: #8ca9e0;">
       </div>
+      <div class="ps-3  mb-3 text-dark fw-bolder menu-sidebar">MENU LAINNYA</div>
+      <a href="" class="link-underline link-underline-opacity-0 text-dark nav-link-sidebar initialColor colorLink sidebar-admin-logout" data-page="page1" id="colorLink">
+            <div class="p-2 ps-4">
+                  <div class="d-flex gap-3 text-danger">
+                        <i class="bi bi-grid-1x2 text-danger"></i>
+                        Keluar
+                  </div>
+            </div>
+      </a>
+     
 </div>
