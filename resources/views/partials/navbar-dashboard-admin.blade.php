4345162953d4cddd98f4d5d34e74e24f97079bd3
unknown <azhardwiandrian@gmail.com>
2024-03-18 08:24:35 +0700

    login

--- resources/views/partials/navbar-dashboard-admin.blade.php
+++ resources/views/partials/navbar-dashboard-admin.blade.php
@@ -1,6 +1,6 @@
 <nav class="navbar sticky-top p-3">
       <div class="container">
-            <a class="navbar-brand text-warning fw-bold" href="/">J4bSeeker</a>
+            <a class="navbar-brand text-warning fw-bold" href="">J4bSeeker</a>
             @yield('nav')
       </div>
 </nav>
