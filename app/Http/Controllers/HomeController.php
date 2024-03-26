2f11dd7b87bdac5bafe23b5cd76fa5c9b0feae7a
kafipratama11 <kafipratama0606@gmail.com>
2024-03-26 15:24:11 +0700

    loker solved

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -20,6 +20,7 @@ class HomeController extends Controller
         $data = loker::all();
         return view('index',compact('data'));
     }
+    
     public function employe(){
         return view('employer/employer-site');
     }
